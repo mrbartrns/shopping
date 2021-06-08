9d4a7a51bd3d1be6f6f619a73d11e537aed8f0e6
junhyuck Ko <mrbartrns@naver.com>
2021-06-08 18:21:47 +0900

    Create Detail.js to seperate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
+import { Link, Route, Switch } from "react-router-dom";
 import {
   Navbar,
   NavDropdown,
@@ -10,13 +11,25 @@ import {
   Jumbotron,
 } from "react-bootstrap";
 import Data from "./data";
+import Detail from "./Detail";
 function App() {
   const [shoes, setShoes] = useState(Data);
   return (
     <div className="App">
       <NavbarWrapper />
-      <JumbotronWrapper />
-      <ShoesContainer shoes={shoes} />
+      <Switch>
+        {/* 여러개가 모두 맞더라도 택1하여 보여줌 */}
+        <Route exact path="/">
+          <JumbotronWrapper />
+          <ShoesContainer shoes={shoes} />
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        <Route path="/:id">
+          <div>아무거나 적었을 때 보여지는 페이지</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
@@ -30,8 +43,12 @@ function NavbarWrapper() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link>
+            <Link to="/">Home</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/detail">Detail</Link>
+          </Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
@@ -57,10 +74,7 @@ function JumbotronWrapper() {
   return (
     <Jumbotron className="jumbo">
       <h1>20% Season OFF 감사제</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
+      <p>이번주에만 잡을 수 있는 특별한 기회를 놓치지 마세요.</p>
       <p>
         <Button variant="primary">Learn more</Button>
       </p>
@@ -83,13 +97,13 @@ function ShoesContainer(props) {
 
 // 계속 상속하여 컴포넌트 구성도 가능
 function ShoesWrapper(props) {
+  const shoes = props.data;
   return (
-    <div className="col-md-4" key={props.data.id}>
-      <img src={props.data.img ? props.data.img : null} />
-      <h4>{props.data.title}</h4>
+    <div className="col-md-4" key={shoes.id}>
+      <img src={shoes.img ? shoes.img : null} />
+      <h4>{shoes.title}</h4>
       <p>
-        {props.data.content}
-        <span className="shoes-price">{props.data.price}</span>
+        {shoes.content} &<span className="shoes-price"> {shoes.price}</span>
       </p>
     </div>
   );
